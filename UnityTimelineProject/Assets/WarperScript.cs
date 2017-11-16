ce0f0d67331fbb83c4412096458a731fdbdd9622
TerrenceChan <roxes2112@yahoo.com>
2017-11-16 20:32:03 +0800

    *Project with cinematic timeline and legacy special effects.

---
+++ UnityTimelineProject/Assets/WarperScript.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityStandardAssets.ImageEffects;
+
+public class WarperScript : MonoBehaviour
+{
+
+	public Vortex vortex;
+
+	void Start ()
+	{
+		
+	}
+
+	void Update ()
+	{
+		Vector3 screenSpacePoint = Camera.main.WorldToViewportPoint (this.transform.position);
+		vortex.center = new Vector2 (screenSpacePoint.x, screenSpacePoint.y);
+	}
+}
